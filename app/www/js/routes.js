ca1b73d251b3f5c284ea2d5c706241f619bf4a58
Markus Roth <Markus.Roth@bbv.ch>
2014-08-06 10:09:00 +0200

    incremented version

--- app/www/js/routes.js
+++ app/www/js/routes.js
@@ -1,31 +1,26 @@
 angular.module('cute', ['ionic', 'cute.services', 'cute.controllers', 'cute.exceptionHandler'])
-
 .config(function ($stateProvider, $urlRouterProvider) {
 
   $stateProvider
-
+    .state('init', {
+      url: '/init',
+      templateUrl: 'templates/init.html',
+      controller: 'initController',
+    })
     .state('list', {
       url: '/list',
       templateUrl: 'templates/list.html',
       controller: 'listController'
     })
-
     .state('match', {
       url: '/match/',
       templateUrl: 'templates/match.html',
       controller: 'matchController',
     })
-
     .state('login', {
       url: '/login',
       templateUrl: 'templates/login.html',
       controller: 'loginController',
-    })
-
-    .state('init', {
-      url: '/init',
-      templateUrl: 'templates/init.html',
-      controller: 'initController',
     });
 
   $urlRouterProvider.otherwise('/init');
